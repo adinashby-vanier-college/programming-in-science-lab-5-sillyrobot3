b427cb66b16f44db0f3ac1ade1bc2f328855053e
Iasmin <156489284+sillyrobot3@users.noreply.github.com>
2026-01-07 08:10:45 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -9,24 +9,30 @@ def hollow_square(n):
 #    newline = "\n"
    x = "*"
    y = " "
-   z = (n - 4) * y
-   new = f"{x}{z}{x}"
+   z = (n - 2) * y
+
+   def new():
+    return f"{x}{z}{x}\n"
+   newer = new() * (n - 2)
    count = 3
-   
-   if n > 1:
+
+   if n == 2:
+    return f"{x * n}\n{x * n}"
+   elif n > 1:
     # result += t
     while count <= n:
-        count += 1   
+        # new1 = new * n
+        count += 1
     # result += t
-    return f"{x * n}\n{new}\n{x * n}"
+    return f"{x * n}\n{newer}{x * n}"
    else:
         return x * n
 #    return hollow_square(n)
 
    
-print(hollow_square(1))
-print(hollow_square(2))
-print(hollow_square(5))
+# print(hollow_square(5))
+# print(hollow_square(2))
+# print(hollow_square(1))
 
 
 # def hollow_square(n):
@@ -42,17 +48,17 @@ print(hollow_square(5))
 # 12
 # 123
 # 1234
-# def number_pattern(n):
-#     return ""
+def number_pattern(n):
+    return ""
 
 # # # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
-# def sum_of_natural_numbers(n):
-#     return ""
+def sum_of_natural_numbers(n):
+    return ""
 
 # # Example for n = 4:
 # #    *
 # #   ***
 # #  *****
 # # *******
-# def centered_star_pyramid(n):
-#     return ""
+def centered_star_pyramid(n):
+    return ""
