214107358e4114e9a228d67fa46f8a95b62c8c21
Iasmin <156489284+sillyrobot3@users.noreply.github.com>
2026-01-09 08:19:02 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,64 +1,72 @@
-# Example for n = 5:
-# *****
-# *   *
-# *   *
-# *   *
-# *****
+
 def hollow_square(n):
-#    result = ""
-#    newline = "\n"
+
    x = "*"
    y = " "
    z = (n - 2) * y
-
+   count = 3
    def new():
     return f"{x}{z}{x}\n"
+
    newer = new() * (n - 2)
-   count = 3
 
    if n == 2:
     return f"{x * n}\n{x * n}"
+
    elif n > 1:
-    # result += t
     while count <= n:
-        # new1 = new * n
         count += 1
-    # result += t
     return f"{x * n}\n{newer}{x * n}"
+
    else:
         return x * n
-#    return hollow_square(n)
+
+   return hollow_square(n)
 
    
-# print(hollow_square(5))
-# print(hollow_square(2))
-# print(hollow_square(1))
+# # print(hollow_square(5))
+# # print(hollow_square(2))
+# # print(hollow_square(1))
 
 
-# def hollow_square(n):
-#    x = "*"
-#    return(x * n)
-#    y = " "
-#    count = 2
-#    while count <= n - 1:
-#     return(x, (n - 4) * y, x)
-#     count += 1
-#    return(x*n)
-# 1
-# 12
-# 123
-# 1234
 def number_pattern(n):
-    return ""
+    result = ""
+    for i in range(n):
+        for j in range(i + 1):
+            result += str(j + 1)
+        result += "\n"
+    return result.strip()
 
-# # # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
+# print(number_pattern(4))
+# print(number_pattern(1))
+# print(number_pattern(3))
+
+
 def sum_of_natural_numbers(n):
-    return ""
+    result = 0
+    count = 1
 
-# # Example for n = 4:
-# #    *
-# #   ***
-# #  *****
-# # *******
+    for i in range(n):
+        result += count + i
+
+    return result
+
+
+# print(sum_of_natural_numbers(5))
+# print(sum_of_natural_numbers(0))
+# print(sum_of_natural_numbers(1))
+# print(sum_of_natural_numbers(10))
+# print(sum_of_natural_numbers(3))
+
+
 def centered_star_pyramid(n):
-    return ""
+    result = ""
+    
+    for i in range(n):
+        for j in range(n - i - 1):
+            result += " "
+        result += str(((i * 2) + 1) * "*")
+        result += "\n"
+    return result.rstrip()
+
+# print(centered_star_pyramid(4))
